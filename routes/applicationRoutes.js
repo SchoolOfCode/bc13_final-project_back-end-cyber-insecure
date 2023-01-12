ead98227e59bace0b2186dd7d311666e50212ba9
Callasaurus <c.sullivandrage@outlook.com>
2023-01-12 15:55:23 +0000

    refactored patch

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -15,23 +15,18 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/", async (req, res) => {
-    if (req.body.progress === null) {
-        const createdApplication = await createApplication(req.body.user_email, req.body.job_title, req.body.company, req.body.job_description, req.body.location, req.body.salary, req.body.job_link, req.body.notes);
-        return res.json({ success: true, payload: createdApplication });
-    } else {
-        const editedApplication = await editApplication(req.params.id, req.body.user_email, req.body.job_title, req.body.company, req.body.job_description, req.body.location, req.body.salary, req.body.job_link, req.body.notes);
-        return res.json({ success: true, payload: editedApplication });
-    }
-}) 
-
-// router.patch("/:id", async (req, res) => {
-//     const editedApplication = await editApplication(req.params.id, req.body.user_email, req.body.job_title, req.body.company, req.body.job_description, req.body.location, req.body.salary, req.body.job_link, req.body.notes);
-//     return res.json({ success: true, payload: editedApplication });
-// })
+    const createdApplication = await createApplication(req.body.user_email, req.body.job_title, req.body.company, req.body.job_description, req.body.location, req.body.salary, req.body.job_link, req.body.notes);
+    return res.json({ success: true, payload: createdApplication });
+})
 
 router.patch("/:id", async (req, res) => {
-    const editedProgress = await editApplicationProgress(req.params.id, req.body.progress);
-    return res.json({ success: true, payload: editedProgress });
+    if (req.body.progress === null) {
+        const editedApplication = await editApplication(req.params.id, req.body.user_email, req.body.job_title, req.body.company, req.body.job_description, req.body.location, req.body.salary, req.body.job_link, req.body.notes);
+        return res.json({ success: true, payload: editedApplication });
+    } else {
+        const editedProgress = await editApplicationProgress(req.params.id, req.body.progress);
+        return res.json({ success: true, payload: editedProgress }); 
+    }
 })
 
 router.delete("/:id", async (req, res) => {

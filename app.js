3acdef2b71e779efda8865ffdc497129b8a6e9e2
Callasaurus <c.sullivandrage@outlook.com>
2023-01-21 17:08:44 +0000

    setting up cv upload feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import router from './routes/applicationRoutes.js'
 import knowledgeRouter from './routes/knowledgeBankRoutes.js'
+import uploadRouter from './routes/uploads.js';
 import express from "express";
 import morgan from "morgan";
 import cors from 'cors'
@@ -11,13 +12,14 @@ const app = express();
 app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use("/api/jobApplications", router);
 app.use("/api/knowledgeBank", knowledgeRouter);
+app.use("/api/files", uploadRouter);
 
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}`);
 });
-
 
 export default app;

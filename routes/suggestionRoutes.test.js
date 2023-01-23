4fab98e3434b3f4fdd6879a54b398c0306fbc18d
Barbara Millon <millon.barbara1@gmail.com>
2023-01-23 11:31:53 +0000

    Routes + models unit tests added for the quiz + suggestion components

---
+++ routes/suggestionRoutes.test.js
@@ -0,0 +1,23 @@
+import { jest } from '@jest/globals'
+
+
+describe("POST /", () => {
+    it("Posts a new suggestion", async () => {
+        const newSuggestion = {
+            cv_link: "",
+            interview_question_type: "",
+            interview_question: "",
+            interview_answer: "",
+            knowledge_topic: "",
+            knowledge_concept: "",
+            github_link: "",
+            portfolio_link: "",
+            quiz_topic: "",
+            quiz_question: ""
+        }
+
+        const postSuggestions = jest.fn();
+        const result = await postSuggestions(newSuggestion);
+        expect(result).toBe();
+    });
+});
